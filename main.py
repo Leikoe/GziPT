5789ffdf2a1ac1dc2e515921d4d854536ec136bf
leikoe <chachafrdark@gmail.com>
2023-08-03 23:13:50 +0200

    working streaming

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import gzip
+import os
+import time
+
 import numpy as np
 import ray
 from ray.experimental import tqdm_ray
@@ -8,12 +11,30 @@ from transformers import LlamaTokenizerFast
 
 tokenizer = LlamaTokenizerFast.from_pretrained("hf-internal-testing/llama-tokenizer")
 
+
+def clearscreen(numlines=100):
+    """
+    Clear the console.
+    numlines is an optional argument used only as a fall-back.
+    """
+    # Thanks to Steven D'Aprano, http://www.velocityreviews.com/forums
+
+    if os.name == "posix":
+        # Unix/Linux/MacOS/BSD/etc
+        os.system('clear')
+    elif os.name in ("nt", "dos", "ce"):
+        # DOS/Windows
+        os.system('CLS')
+    else:
+        # Fallback for other operating systems.
+        print('\n' * numlines)
+
+
 # Start Ray. This creates some processes that can do work in parallel.
 ray.init()
 
 # hyperparameters
-context_size = 16 # what is the maximum context length for predictions?
-
+context_size = 16  # what is the maximum context length for predictions?
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 with open('input.txt', 'r', encoding='utf-8') as f:
@@ -39,11 +60,12 @@ n = int(0.9 * len(data))  # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
 
+
 # data loading
 def get_data(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == 'train' else val_data
-    ix = np.array(range(0, len(data) - context_size)) #TODO: is step a good idea ?
+    ix = np.array(range(0, len(data) - context_size))  # TODO: is step a good idea ?
     x = np.stack([data[i:i + context_size] for i in ix])
     y = np.stack([data[i + 1:i + context_size + 1] for i in ix])
     return x, y
@@ -60,7 +82,7 @@ for x, y in list(zip(*get_data("train"))):
     for token_idx in range(context_size):
         context = x[:token_idx + 1]
         target = y[token_idx]
-        print(f"when context is {list(context)}, target is {target}")
+        # print(f"when context is {list(context)}, target is {target}")
         X.append([context.tobytes(), len(gzip.compress(context.tobytes()))])
         Y.append(target)
 
@@ -80,13 +102,15 @@ remote_tqdm = ray.remote(tqdm_ray.tqdm)
 bar = remote_tqdm.remote(total=len(X), desc="creating model")
 train_ncd = ray.get([nomnom_fast.remote(bar, i) for i in range(len(X))])
 bar.close.remote()
+time.sleep(0.1)
 ray.shutdown()
 
 
 def generate(knn: KNeighborsClassifier, context: np.ndarray, max_new_tokens: int):
+    clearscreen()
+    print(tokenizer.decode(context), end="")
     # idx is (B, T) array of indices in the current context
-    for _ in tqdm.tqdm(range(max_new_tokens), desc="generation"):
-        # print(decode(context))
+    for _ in range(max_new_tokens):
         # crop idx to the last block_size tokens
         idx_cond = context[-context_size:]
         idx_cond_compressed = len(gzip.compress(idx_cond.tobytes()))
@@ -98,6 +122,7 @@ def generate(knn: KNeighborsClassifier, context: np.ndarray, max_new_tokens: int
         idx_next = knn.predict([ncd_scores])
         # append sampled index to the running sequence
         context = np.concatenate((context, idx_next))  # (B, T+1)
+        print(tokenizer.decode(idx_next), end=" ")
     return context
 
 

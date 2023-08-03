aa0063045578693ecf168cfa39ee96802e2d8535
leikoe <chachafrdark@gmail.com>
2023-08-03 22:28:25 +0200

    working llama2 tokenizer

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import ray
 from ray.experimental import tqdm_ray
 import tqdm
 from sklearn.neighbors import KNeighborsClassifier
-from transformers import GPT2Tokenizer
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+from transformers import LlamaTokenizerFast
+
+tokenizer = LlamaTokenizerFast.from_pretrained("hf-internal-testing/llama-tokenizer")
 
 # Start Ray. This creates some processes that can do work in parallel.
 ray.init()
 
 # hyperparameters
-block_size = 4 # what is the maximum context length for predictions?
+context_size = 16 # what is the maximum context length for predictions?
 
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
@@ -30,9 +31,10 @@ with open('input.txt', 'r', encoding='utf-8') as f:
 # decode = lambda l: ''.join([itos[i] for i in l])  # decoder: take a list of integers, output a string
 
 # Train and test splits
-data = np.array(tokenizer.encode(text, add_special_tokens=False), dtype=np.int64)
+data = np.array(tokenizer.encode(text), dtype=np.int64)
 print("training data length:", len(data))
 print("vocab size:", tokenizer.vocab_size)
+
 n = int(0.9 * len(data))  # first 90% will be train, rest val
 train_data = data[:n]
 val_data = data[n:]
@@ -41,9 +43,9 @@ val_data = data[n:]
 def get_data(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == 'train' else val_data
-    ix = np.array(range(0, len(data) - block_size)) #TODO: is step a good idea ?
-    x = np.stack([data[i:i + block_size] for i in ix])
-    y = np.stack([data[i + 1:i + block_size + 1] for i in ix])
+    ix = np.array(range(0, len(data) - context_size)) #TODO: is step a good idea ?
+    x = np.stack([data[i:i + context_size] for i in ix])
+    y = np.stack([data[i + 1:i + context_size + 1] for i in ix])
     return x, y
 
 
@@ -55,7 +57,7 @@ def ncd_fast(x: bytes, x_compressed: int, x2: bytes, x2_compressed: int):  # NCD
 X = []
 Y = []
 for x, y in list(zip(*get_data("train"))):
-    for token_idx in range(block_size):
+    for token_idx in range(context_size):
         context = x[:token_idx + 1]
         target = y[token_idx]
         print(f"when context is {list(context)}, target is {target}")
@@ -86,13 +88,14 @@ def generate(knn: KNeighborsClassifier, context: np.ndarray, max_new_tokens: int
     for _ in tqdm.tqdm(range(max_new_tokens), desc="generation"):
         # print(decode(context))
         # crop idx to the last block_size tokens
-        idx_cond = context[-block_size:]
+        idx_cond = context[-context_size:]
         idx_cond_compressed = len(gzip.compress(idx_cond.tobytes()))
         # get the predictions
         ncd_scores = np.array([ncd_fast(idx_cond.tobytes(), idx_cond_compressed, *x2) for x2 in X])
-        probs = knn.predict_proba([ncd_scores])
+        # probs = knn.predict_proba([ncd_scores])
         # sample from the distribution
-        idx_next = np.random.choice(probs.shape[1], 1, p=probs[0])  # (B, 1)
+        # idx_next = np.random.choice(probs.shape[1], 1, p=probs[0])  # (B, 1)
+        idx_next = knn.predict([ncd_scores])
         # append sampled index to the running sequence
         context = np.concatenate((context, idx_next))  # (B, T+1)
     return context
@@ -101,6 +104,6 @@ def generate(knn: KNeighborsClassifier, context: np.ndarray, max_new_tokens: int
 neigh = KNeighborsClassifier(n_neighbors=3)
 neigh.fit(train_ncd, Y)
 
-context = np.array(tokenizer.encode("Citizen"), dtype=np.int64)
+context = np.array([tokenizer.bos_token_id], dtype=np.int64)
 print(context)
 open('output.txt', 'w').write(tokenizer.decode(generate(neigh, context, max_new_tokens=200)))
